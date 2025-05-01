0c5dd14dbaa09aec4f3e6fb4417ce0c53851d8c5
JGaldp-beep <juan.galdo@ucb.edu.bo>
2025-04-30 21:30:17 -0400

    Code for learning

--- app/src/main/java/com/example/todoapp/navigation/TodoNavGraph.kt
+++ app/src/main/java/com/example/todoapp/navigation/TodoNavGraph.kt
@@ -3,14 +3,8 @@ package com.example.todoapp.navigation
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
-import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.navArgument
-import com.example.todoapp.ui.screens.detail.DetailDestination
-import com.example.todoapp.ui.screens.detail.DetailScreen
-import com.example.todoapp.ui.screens.edit.EditDestination
-import com.example.todoapp.ui.screens.edit.EditScreen
 import com.example.todoapp.ui.screens.entry.EntryDestination
 import com.example.todoapp.ui.screens.entry.EntryScreen
 import com.example.todoapp.ui.screens.home.HomeScreen
@@ -35,15 +29,6 @@ fun TodoNavGraph(
         }
 
         composable(
-            route = DetailDestination.routeWithArgs,
-            arguments = listOf(navArgument(DetailDestination.todoIdArg) {
-                type = NavType.IntType
-            })
-        ) {
-            DetailScreen()
-        }
-
-        composable(
             route = EntryDestination.route
         ) {
             EntryScreen(
@@ -54,12 +39,6 @@ fun TodoNavGraph(
                     navController.navigateUp()
                 }
             )
-        }
-
-        composable(
-            route = EditDestination.routeWithArgs
-        ) {
-            EditScreen()
         }
     }
 }
--- app/src/main/java/com/example/todoapp/ui/AppViewModelProvider.kt
+++ app/src/main/java/com/example/todoapp/ui/AppViewModelProvider.kt
@@ -2,13 +2,10 @@ package com.example.todoapp.ui
 
 import android.app.Application
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory
-import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.todoapp.TodoApplication
-import com.example.todoapp.ui.screens.detail.DetailViewModel
-import com.example.todoapp.ui.screens.edit.EditViewModel
 import com.example.todoapp.ui.screens.entry.EntryViewModel
 import com.example.todoapp.ui.screens.home.HomeViewModel
 
@@ -17,22 +14,6 @@ import com.example.todoapp.ui.screens.home.HomeViewModel
  */
 object AppViewModelProvider {
     val Factory = viewModelFactory {
-        // Initializer for DetailViewModel
-        initializer {
-            DetailViewModel(
-                this.createSavedStateHandle(),
-                todoApplication().container.todoRepository
-            )
-        }
-
-        // Initializer for EditViewModel
-        initializer {
-            EditViewModel(
-                this.createSavedStateHandle(),
-                todoApplication().container.todoRepository
-            )
-        }
-
         // Initializer for EntryViewModel
         initializer {
             EntryViewModel(
